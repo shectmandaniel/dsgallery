c7b71df2bf8fa0ef5a544cc6f2b2fd9db5c61807
Daniel Shechtman <shechtmandaniel@gmail.com>
2024-04-17 11:50:40 +0300

    Refactor HomePage component to separate Images fetching and rendering, add authentication checks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,39 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
+async function Images() {
   const images = await db.query.images.findMany({
-    orderBy: (model, {desc}) => desc(model.id),
+    orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="h-auto w-48">
+          <img
+            src={image.url}
+            alt="mock"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        
-        {images.map((image) => (
-          <div
-            key={image.id}
-            className="h-auto w-48"
-          >
-            <img
-              src={image.url}
-              alt="mock"
-              className="object-cover w-full h-full"
-            />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl p-3">
+          Please Sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
