b1665054cc9c22618a80dbf6a6f070bebf4f7e4f
Daniel Shechtman <shechtmandaniel@gmail.com>
2024-04-17 19:29:39 +0300

    Use Nextjs Image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { getMyImages } from "~/server/db/queeries";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
 async function Images() {
   const images = await getMyImages();
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap gap-4 justify-center">
       {images.map((image) => (
-        <div key={image.id} className="h-auto w-48">
-          <img
+        <div key={image.id} className="size-48">
+          <Image
             src={image.url}
-            alt="mock"
-            className="h-full w-full object-cover"
+            alt={image.name}
+            width={192}
+            height={192}
+            className="size-full object-cover rounded-md"
           />
         </div>
       ))}
